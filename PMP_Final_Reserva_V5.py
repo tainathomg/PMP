0f5ab3abe0b2d6c09cb71ecc37f1fa6407fee321
tainathomg <65792044+tainathomg@users.noreply.github.com>
2025-05-13 12:07:52 -0300

    Update PMP_Final_Reserva_V5.py

--- PMP_Final_Reserva_V5.py
+++ PMP_Final_Reserva_V5.py
@@ -1044,7 +1044,7 @@ def create_dashboard(amostras_df, base_path):
     with open(output_path, 'r', encoding='utf-8') as f:
         html_content = f.read()
 
-    st.components.v1.html(html_content, height=1000, width=1400, scrolling=True)
+    st.components.v1.html(html_content, height=1000, scrolling=True)
   
 
   
